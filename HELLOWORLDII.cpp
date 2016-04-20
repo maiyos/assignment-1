41513f37e4f48361a5c9aae6c7c074b2c7727245
MAIYO2806 <denis.maiyo@outlook.com>
2016-04-20 19:31:37 +0300

    Added files via upload

---
+++ HELLOWORLDII.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+int N;
+cin >> N;
+for
+(;N-->0;)
+{
+cout <<"hello,World!\n";
+}
+return 0;
+}
---
+++ whileloop.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+int N;
+cin >> N;
+while(N-- >0);
+{
+    cout << "hello,world!\n";
+}
+return  0;
+}
